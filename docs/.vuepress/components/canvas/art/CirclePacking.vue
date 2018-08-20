2f30967e51cc1e70feb1304f201efda70352afa6
Dang Van Thanh <dangvanthanh@dangthanh.org>
2018-08-20 22:54:15 +0700

    Improve circle packing canvas

--- docs/.vuepress/components/canvas/art/CirclePacking.vue
+++ docs/.vuepress/components/canvas/art/CirclePacking.vue
@@ -7,37 +7,44 @@ import { randHsl } from './Utils';
 
 export default {
   name: 'canvas-art-CirclePacking',
+  data() {
+    return {
+      canvas: '',
+      context: '',
+      circles: [],
+      size: 0
+    };
+  },
   mounted() {
-    let canvas = this.$el;
-    let context = canvas.getContext('2d');
-    let size = window.innerWidth / 2;
-    let squareSize = 40;
-    let randomDisplacement = 15;
-    let rotateMultiplier = 20;
-
-    context.lineWidth = 2;
-
-    canvas.width = size;
-    canvas.height = size;
-
-    let circles = [];
-    let minRadius = 2;
-    let maxRadius = 100;
+    this.canvas = this.$el;
+    this.context = this.canvas.getContext('2d');
+    this.size = window.innerWidth / 2;
+    this.context.lineWidth = 2;
+    this.canvas.width = this.size;
+    this.canvas.height = this.size;
     let totalCircles = 500;
-    let createCircleAttempts = 500;
 
-    function createAndDrawCircle() {
+    for (let i = 0; i < totalCircles; i++) {
+      this.createAndDrawCircle();
+    }
+  },
+  methods: {
+    createAndDrawCircle() {
+      let minRadius = 2;
+      let maxRadius = 100;
+      let createCircleAttempts = 500;
+
       let newCircle;
       let circleSafeToDraw = false;
 
       for (let tries = 0; tries < createCircleAttempts; tries++) {
         newCircle = {
-          x: Math.floor(Math.random() * size),
-          y: Math.floor(Math.random() * size),
+          x: Math.floor(Math.random() * this.size),
+          y: Math.floor(Math.random() * this.size),
           radius: minRadius
         };
 
-        if (doesCircleHaveACollision(newCircle)) {
+        if (this.doesCircleHaveACollision(newCircle)) {
           continue;
         } else {
           circleSafeToDraw = true;
@@ -51,21 +58,26 @@ export default {
 
       for (let radiusSize = minRadius; radiusSize < maxRadius; radiusSize++) {
         newCircle.radius = radiusSize;
-        if (doesCircleHaveACollision(newCircle)) {
+        if (this.doesCircleHaveACollision(newCircle)) {
           newCircle.radius--;
           break;
         }
       }
 
-      circles.push(newCircle);
-      context.beginPath();
-      context.arc(newCircle.x, newCircle.y, newCircle.radius, 0, 2 * Math.PI);
-      context.stroke();
-    }
-
-    function doesCircleHaveACollision(circle) {
-      for (let i = 0; i < circles.length; i++) {
-        let otherCircle = circles[i];
+      this.circles.push(newCircle);
+      this.context.beginPath();
+      this.context.arc(
+        newCircle.x,
+        newCircle.y,
+        newCircle.radius,
+        0,
+        2 * Math.PI
+      );
+      this.context.stroke();
+    },
+    doesCircleHaveACollision(circle) {
+      for (let i = 0; i < this.circles.length; i++) {
+        let otherCircle = this.circles[i];
         let a = circle.radius + otherCircle.radius;
         let x = circle.x - otherCircle.x;
         let y = circle.y - otherCircle.y;
@@ -75,19 +87,21 @@ export default {
         }
       }
 
-      if (circle.x + circle.radius >= size || circle.x - circle.radius <= 0) {
+      if (
+        circle.x + circle.radius >= this.size ||
+        circle.x - circle.radius <= 0
+      ) {
         return true;
       }
 
-      if (circle.y + circle.radius >= size || circle.y - circle.radius <= 0) {
+      if (
+        circle.y + circle.radius >= this.size ||
+        circle.y - circle.radius <= 0
+      ) {
         return true;
       }
 
       return false;
-    }
-
-    for (let i = 0; i < totalCircles; i++) {
-      createAndDrawCircle();
     }
   }
 };
