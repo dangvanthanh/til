c9793cd51340b6309d7e9e41d3738327d473a664
Dang Van Thanh <dangvanthanh@dangthanh.org>
2023-08-24 23:59:15 +0700

    Add alpine and lit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,4 @@
 import { defineConfig } from 'astro/config';
-
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
 import partytown from "@astrojs/partytown";
@@ -8,11 +7,14 @@ import solidJs from "@astrojs/solid-js";
 import svelte from "@astrojs/svelte";
 import vue from "@astrojs/vue";
 import mdx from "@astrojs/mdx";
+import alpinejs from "@astrojs/alpinejs";
+
+import lit from "@astrojs/lit";
 
 // https://astro.build/config
 export default defineConfig({
   experimental: {
     viewTransitions: true
   },
-  integrations: [react(), tailwind(), partytown(), preact(), solidJs(), svelte(), vue(), mdx()]
+  integrations: [react(), tailwind(), partytown(), preact(), solidJs(), svelte(), vue(), mdx(), alpinejs(), lit()]
 });
---
+++ src/components/LitComponent.ts
@@ -0,0 +1,9 @@
+import { LitElement, html } from "lit";
+
+export class LitComponent extends LitElement {
+  render() {
+    return html`<div>Lit Component</div>`;
+  }
+}
+
+customElements.define("lit-component", LitComponent);
