ab704c199df5edfc00616dce5f803615846b8825
Dang Van Thanh <dangvanthanh@dangthanh.org>
2024-09-30 23:52:58 +0700

    deps: update astro dependencies

--- astro.config.mjs
+++ astro.config.mjs
@@ -15,54 +15,54 @@ import rehypePrettyCode from 'rehype-pretty-code'
 import moonlightTheme from './public/theme/moonlight-ii.json'
 
 const options = {
-  // Specify the theme to use or a custom theme json, in our case
-  // it will be a moonlight-II theme from
-  // https://github.com/atomiks/moonlight-vscode-theme/blob/master/src/moonlight-ii.json
-  theme: moonlightTheme,
-  // Callbacks to customize the output of the nodes
-  onVisitLine(node) {
-    // Prevent lines from collapsing in `display: grid` mode, and
-    // allow empty lines to be copy/pasted
-    if (node.children.length === 0) {
-      node.children = [
-        {
-          type: 'text',
-          value: ' ',
-        },
-      ]
-    }
-  },
-  onVisitHighlightedLine(node) {
-    // Adding a class to the highlighted line
-    node.properties.className.push('highlighted')
-  },
+	// Specify the theme to use or a custom theme json, in our case
+	// it will be a moonlight-II theme from
+	// https://github.com/atomiks/moonlight-vscode-theme/blob/master/src/moonlight-ii.json
+	theme: moonlightTheme,
+	// Callbacks to customize the output of the nodes
+	onVisitLine(node) {
+		// Prevent lines from collapsing in `display: grid` mode, and
+		// allow empty lines to be copy/pasted
+		if (node.children.length === 0) {
+			node.children = [
+				{
+					type: 'text',
+					value: ' ',
+				},
+			]
+		}
+	},
+	onVisitHighlightedLine(node) {
+		// Adding a class to the highlighted line
+		node.properties.className.push('highlighted')
+	},
 }
 
 // https://astro.build/config
 export default defineConfig({
-  markdown: {
-    syntaxHighlight: false,
-    rehypePlugins: [[rehypePrettyCode, options]],
-  },
-  integrations: [
-    react({
-      include: ['**/react/*'],
-    }),
-    partytown(),
-    preact({
-      include: ['**/preact/*'],
-    }),
-    solidJs({
-      include: ['**/solid/*'],
-    }),
-    svelte(),
-    vue(),
-    mdx(),
-    alpinejs(),
-    lit(),
-    markdoc(),
-    keystatic(),
-  ],
-  output: 'hybrid',
-  adapter: vercel(),
+	markdown: {
+		syntaxHighlight: false,
+		rehypePlugins: [[rehypePrettyCode, options]],
+	},
+	integrations: [
+		react({
+			include: ['**/react/*'],
+		}),
+		preact({
+			include: ['**/preact/*'],
+		}),
+		solidJs({
+			include: ['**/solid/*'],
+		}),
+		svelte(),
+		vue(),
+		alpinejs(),
+		lit(),
+		mdx(),
+		partytown(),
+		markdoc(),
+		keystatic(),
+	],
+	output: 'hybrid',
+	adapter: vercel(),
 })
